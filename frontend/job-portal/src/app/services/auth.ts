79257acffe009d207adc1e3063c8f7a40436b92d
DJEUFACKWILSON <wilsondjeufack@gmail.com>
2026-06-12 08:18:50 +0200

    Fix import path for environment in auth service

--- frontend/job-portal/src/app/services/auth.ts
+++ frontend/job-portal/src/app/services/auth.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, tap } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { User, LoginRequest, RegisterRequest, AuthResponse } from '../models/user';
 
 @Injectable({
